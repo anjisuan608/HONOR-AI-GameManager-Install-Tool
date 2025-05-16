e060ceccf7a30cf8932070b041f7040c79f22894
anjisuan608 <anjisuan608@petalmail.com>
2025-05-16 17:46:24 +0800

    更新了一些功能

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # 荣耀AI游戏管家安装工具
+## 存储库
+___若访问缓慢可以尝试前往以下站点:___
+- GitHub:&ensp;[项目仓库](https://github.com/anjisuan608/HONOR-AI-GameManager-Install-Tool)&emsp;[文件下载](https://github.com/anjisuan608/HONOR-AI-GameManager-Install-Tool/releases)
+- GitCode:&ensp;[项目仓库](https://gitcode.com/anjisuan608/HONOR-AI-GameManager-Install-Tool)&emsp;[文件下载](https://gitcode.com/anjisuan608/HONOR-AI-GameManager-Install-Tool/releases)
+- Gitee:&ensp;[项目仓库](https://gitee.com/anjisuan608/HONOR-AI-GameManager-Install-Tool)&emsp;[文件下载](https://gitee.com/anjisuan608/HONOR-AI-GameManager-Install-Tool/releases)
+- AtomGit:&ensp;[项目仓库](https://atomgit.com/anjisuan608/HONOR-AI-GameManager-Install-Tool)&emsp;[文件下载](https://atomgit.com/anjisuan608/HONOR-AI-GameManager-Install-Tool/tags?tab=release)
+
 ## 简介
 
 荣耀AI游戏管家安装工具，用于在尚未完成适配的荣耀笔记本设备上，尝鲜体验荣耀AI游戏管家。
@@ -9,5 +16,5 @@
 
 ## 声明
 
-1. 本项目仅供学习研究静默安装原理，请勿用于商业用途，项目[许可证](./LICENSE)
-2. 本项目存在一定风险，请勿使用于生产环境中！造成事故或经济损失，由使用者自行承担！
+1. **本项目仅供学习研究静默安装原理，请勿用于商业用途，项目[许可证](./LICENSE)**
+2. **本项目存在一定风险，请勿使用于生产环境中！造成事故或经济损失，由使用者自行承担！**
--- installer.cmd
+++ installer.cmd
@@ -9,10 +9,14 @@ echo 荣耀AI游戏管家安装工具
 echo =========================
 echo 键入 i 执行静默安装
 echo 键入 s 执行服务检查
+echo 键入 u 执行静默卸载
+echo 键入 t 清理缓存目录
 echo 键入 x 退出
 echo =========================
-choice /C isx /CS
-if errorlevel 3 goto x
+choice /C isutx /CS
+if errorlevel 5 goto x
+if errorlevel 4 goto t
+if errorlevel 3 goto u
 if errorlevel 2 goto s
 if errorlevel 1 goto i
 echo 无效的输入，请重新选择。
@@ -21,14 +25,14 @@ goto c
 :i
 @REM 静默安装
 if exist "%temp%\OpenVanilla\1_0_0_20_SP2.zip" (
-    echo goto unzippackage
+    echo goto UnZipPackage
 ) else (
     echo 开始下载
     powershell -Command "Invoke-WebRequest -Uri 'https://iknow-dl.service.hihonor.com/ctkbfm/servlet/download/downloadServlet/H4sIAAAAAAAAAD1QTWuEMBT8KyWnFmSJSfzaU6OupQe7C27psTxNtKF-LE-tdEv_eyPY5R0eM8wMw_yQedR4_r5osieMOEQNS79B38LatPoFuhU-2ZdDD43GO3dH7TF6X5xYP7RD8_CeGewWQL27mstmPMH0YY2cBlBy4Bx8JYB5pScUVKXvQliLOgSrLs31WVlpcXx8o5QKEdHA0hVqmMzQn83agDpkNE0P04xrH85kkIY8iQ4yyTwRRQmncSjcg5RpLCTzAxnKTCScez4NwjSiPuORy9OUcTdNYpu_wKQxB_zMWmjIvp_b1iEaccB8vOEOKqkU6nH8Z2zbbaLiaFO-oDXq9TbjhLP-_QNxsjnbWAEAAA%3D%3D.zip' -OutFile '%temp%\OpenVanilla\1_0_0_20_SP2.zip'"
     echo 下载结束
 )
 
-:unzippackage
+:UnZipPackage
 @REM 解压外部包
 if exist "%temp%\OpenVanilla\1_0_0_20_SP2.zip" (
     echo 文件存在，开始解压
@@ -59,17 +63,64 @@ echo.
 goto c
 
 :s
-@REM 服务检查
+@REM 服务存在性检查
 echo 开始服务检查
 powershell -Command "Get-Service -Name 'GameManagerService'"
 if %errorlevel%==0 (
     echo 服务已安装
+    goto ServiceStatus
 ) else (
     echo 服务未安装
    timeout /t 3
    echo.
    goto c
 )
+
+:ServiceStatus
+@REM 检查服务启动状态检查
+powershell -Command "Get-Service -Name 'GameManagerService' | Select-Object -Property Status"
+if %errorlevel%==0 (
+    echo 服务已启动
+    goto c
+) else (
+    echo 服务未启动
+    timeout /t 3
+    echo.
+    goto ServiceStart
+)
+
+:ServiceStart
+@REM 启动服务
+powershell -Command "Start-Service -Name 'GameManagerService'"
+goto c
+
+:u
+@REM 卸载
+@REM 检查是否安装
+if exist "%programfiles%\GameManager\uninst.exe" (
+    echo 卸载程序存在，开始卸载
+    goto uninstall
+) else (
+    echo 卸载程序不存在，请检查安装路径。
+    timeout /t 3
+    echo.
+    goto c
+)
+
+:uninstall
+echo 正在尝试拉起卸载进程
+powershell -Command "Start-Process -FilePath '%programfiles%\GameManager\uninst.exe' -ArgumentList '/S /uninstall' -Wait"
+echo 卸载结束
+echo.
+goto c
+
+:t
+@REM 清理缓存目录
+echo 正在清理缓存目录
+del /q "%temp%\OpenVanilla\*"
+for /d %%i in ("%temp%\OpenVanilla\*") do rd /s /q "%%i"
+echo 缓存已清理
+echo.
 goto c
 
 :x
