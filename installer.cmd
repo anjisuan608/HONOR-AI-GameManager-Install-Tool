e43727f3a80de529d1f41f413c1b11bca5711994
anjisuan608 <anjisuan608@petalmail.com>
2025-08-20 00:05:47 +0800

    更新下载链接到1.0.0.28

--- installer.cmd
+++ installer.cmd
@@ -15,13 +15,18 @@ if %errorlevel% neq 0 (
 @REM 创建临时文件夹
 if not exist "%temp%\OpenVanilla" (
     mkdir "%temp%\OpenVanilla"
+)
+
+@REM 创建数据文件夹
+if not exist "%AppData%\OpenVanilla\HnGMCfg" (
+    mkdir "%AppData%\OpenVanilla\HnGMCfg"
 ) else (
     goto ve
 )
 
 :ve
 @REM 定义版本号
-set "version=1.0.0.26"
+set "version=1.0.0.28"
 goto c
 
 :c
@@ -55,8 +60,8 @@ goto c
 
 :i
 @REM 静默安装
-set "version=1.0.0.26"
-set "downloadUrl=https://iknow-dl.service.hihonor.com/ctkbfm/servlet/download/downloadServlet/H4sIAAAAAAAAAD1QTU-DQBD9K2ZPmpBmKAsLPVnYxXjAmrTGo5mWATfy0QxgY43_3cXUZg4vb_LefLxvMQ3Eu68jiZVYCk-U_am70MjRyjb0hO1MHxwU2GFNfOMvwNUyuu36pq_v3nLL7QmZFmd7vLiecXx3LkV7IAlYoQMpCZUPVQxRFSooD1Xs1Ht7fiyddLu5fwUAGcUQuvaBCUfbdzs7rwdPDLbucJx4PgZkoHS4BJX4xkjQaZiFUQ4mMrGUiTZ6DaAgTBMtU5OZdRDnmQoynWgfcpMbN_-EI3GB_JE3WItVNzWNJ4i552K48hYP67JkGob_zic2tny5hjbyRH8vXELbbsTPL4qz089VAQAA.zip"
+set "version=1.0.0.28"
+set "downloadUrl=https://iknow-dl.service.hihonor.com/ctkbfm/servlet/download/downloadServlet/H4sIAAAAAAAAAD1QTU-EMBD9K6YnTcim0NKWPQllMR5wTXaNRzMLBRv52AwgcY3_3WJwM4fJm7w3b-Z9k2kwePw6G7IlAfFI2c_dCoWDlW3ME7QLfHAthw5qgzf-hroK1G3XN31995ZZbGdAs7nY86p6hvHdqZisWEFpJGVouApBncJAgfCrKBSiAN-xT_byWDrqYX__SinlivPFukADo-27o13sqUcGW3cwTrgco4RMYx1IlfAok5LrJE6oZqlWUaCFzHjo83SnUxowR82EYGzHfCmoyJjUPHX7ZxgN5oAfWQM12XZT03jEIPaYD1fcQhGXJZph-J98QmPLl2toI07m74U1tMOe_PwCOwyzElUBAAA%3D.zip"
 :mi
 echo =========================
 echo 当前目标版本 %version%
@@ -83,8 +88,10 @@ goto ClearDirectoryChoice
 
 :ClearDirectory
 @REM 清空目录内容但保留缓存目录
-del /q "%temp%\OpenVanilla\*"
-for /d %%i in ("%temp%\OpenVanilla\*") do rd /s /q "%%i"
+if exist "%temp%\OpenVanilla" (
+    del /q "%temp%\OpenVanilla\*"
+    for /d %%i in ("%temp%\OpenVanilla\*") do rd /s /q "%%i"
+)
 
 :SkipClear
 
@@ -124,6 +131,12 @@ if exist "%temp%\OpenVanilla\Software\GameManager_Setup_%version%.zip" (
 echo 开始安装
 powershell -Command "Start-Process -FilePath '%temp%\OpenVanilla\GameManager_Setup_%version%.exe' -ArgumentList '/S' -Wait"
 echo 安装结束
+copy "%xmlFile%" "%AppData%\OpenVanilla\FrameCollectConfig-%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%.xml"
+if exist "%AppData%\OpenVanilla\FrameCollectConfig-latest.xml" (
+    type %xmlFile% > "%AppData%\OpenVanilla\FrameCollectConfig-latest.xml"
+) else (
+    copy %xmlFile% "%AppData%\OpenVanilla\FrameCollectConfig-latest.xml"
+)
 echo.
 goto c
 
@@ -301,24 +314,120 @@ goto c
 :EditXml
 @REM ====== 编辑游戏管家支持游戏列表模块 ======
 echo =========================
+echo 键入 b 管理配置文件 FrameCollectConfig.xml
 echo 键入 s 使用系统默认方式打开
 echo 键入 n 使用记事本打开
 echo 键入 q 返回上一级菜单
 echo =========================
-choice /C snq /CS
-if %errorlevel% == 3 goto c
-if %errorlevel% == 2 (
+choice /C bsnq /CS
+if %errorlevel% == 4 goto c
+if %errorlevel% == 3 (
     echo 正在用记事本打开 FrameCollectConfig.xml...
     start notepad "%xmlFile%"
     goto c
 )
-if %errorlevel% == 1 (
+if %errorlevel% == 2 (
     echo 正在用系统默认方式打开 FrameCollectConfig.xml...
     start "" "%xmlFile%"
     goto c
 )
+if %errorlevel% == 1 goto BackupConfigManager
 echo 无效的输入，请重新选择。
 goto EditXml
+
+:BackupConfigManager
+@REM 备份配置文件管理
+echo ========================
+echo 键入 b 备份配置文件
+echo 键入 r 还原配置文件
+echo 键入 q 返回上一级菜单
+echo ========================
+choice /C brq /CS
+if %errorlevel% == 3 goto e
+if %errorlevel% == 2 goto RestoreConfigManagerChoice
+if %errorlevel% == 1 goto BackupConfigManagerChoice
+@REM 错误回收
+echo 输入参数错误。
+goto BackupConfigManager
+
+:BackupConfigManagerChoice
+@REM 备份配置文件选项
+echo ========================
+echo 键入 y 确认备份配置文件
+echo 键入 n 返回上一级菜单
+echo ========================
+choice /C yn /CS
+if %errorlevel% == 2 goto e
+if %errorlevel% == 1 goto BackupConfigManagerShell
+@REM 错误回收
+echo 输入参数错误。
+goto BackupConfigManagerChoice
+
+:BackupConfigManagerShell
+@REM 备份配置文件
+echo 正在备份配置文件
+copy "%xmlFile%" "%AppData%\OpenVanilla\HnGMCfg\FrameCollectConfig-%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%.xml"
+if %errorlevel% neq 0 (
+    echo 备份失败，请检查权限。
+)
+if exist "%AppData%\OpenVanilla\HnGMCfg\FrameCollectConfig-latest.xml" (
+    type "%xmlFile%" > "%AppData%\OpenVanilla\HnGMCfg\FrameCollectConfig-latest.xml"
+) else (
+    copy "%xmlFile%" "%AppData%\OpenVanilla\HnGMCfg\FrameCollectConfig-latest.xml"
+)
+if %errorlevel% neq 0 (
+    echo 备份失败，请检查权限。
+)
+echo 备份完成
+echo.
+goto BackupConfigManager
+
+:RestoreConfigManagerChoice
+@REM 还原配置文件选项
+echo ========================
+echo 键入 y 确认还原配置文件
+echo 键入 n 返回上一级菜单
+echo ========================
+choice /C yn /CS
+if %errorlevel% == 2 goto e
+if %errorlevel% == 1 goto RestoreConfigLocationChoice
+@REM 错误回收
+echo 输入参数错误。
+goto RestoreConfigManagerChoice
+
+:RestoreConfigLocationChoice
+@REM 还原配置文件位置选择
+echo ========================
+echo 键入 l 从最近的备份还原
+echo 键入 o 从其它时间的备份还原
+echo 键入 b 返回上一级菜单
+echo ========================
+choice /C lob /CS
+if %errorlevel% == 3 goto e
+if %errorlevel% == 2 goto RestoreConfigBackupHistory
+if %errorlevel% == 1 set "restoreFile=%AppData%\OpenVanilla\HnGMCfg\FrameCollectConfig-latest.xml" && goto RestoreConfigManagerShell
+@REM 错误回收
+echo 输入参数错误。
+goto RestoreConfigLocationChoice
+
+:RestoreConfigBackupHistory
+@REM 还原配置文件历史
+echo ========================
+echo 功能尚未实现
+echo ========================
+pause
+goto RestoreConfigLocationChoice
+
+:RestoreConfigManagerShell
+@REM 还原配置文件
+echo 正在还原配置文件
+type %restoreFile% > "%xmlFile%"
+if %errorlevel% neq 0 (
+    echo 还原失败，请检查权限。
+)
+echo 还原完成
+echo.
+goto e
 
 :m
 @REM 安装其它版本
@@ -329,6 +438,7 @@ echo 旧版不再维护,链接可能失效!
 echo 0 - 1.0.0.20(SP2)
 echo 1 - 1.0.0.23
 echo 2 - 1.0.0.26
+echo 3 - 1.0.0.28
 echo =========================
 choice /C 0123456789ABCDEF /CS /M "请选择(0~F):"
 if %errorlevel% == 1 (
@@ -346,7 +456,10 @@ if %errorlevel% == 3 (
     set "downloadUrl=https://iknow-dl.service.hihonor.com/ctkbfm/servlet/download/downloadServlet/H4sIAAAAAAAAAD1QTU-DQBD9K2ZPmpBmKAsLPVnYxXjAmrTGo5mWATfy0QxgY43_3cXUZg4vb_LefLxvMQ3Eu68jiZVYCk-U_am70MjRyjb0hO1MHxwU2GFNfOMvwNUyuu36pq_v3nLL7QmZFmd7vLiecXx3LkV7IAlYoQMpCZUPVQxRFSooD1Xs1Ht7fiyddLu5fwUAGcUQuvaBCUfbdzs7rwdPDLbucJx4PgZkoHS4BJX4xkjQaZiFUQ4mMrGUiTZ6DaAgTBMtU5OZdRDnmQoynWgfcpMbN_-EI3GB_JE3WItVNzWNJ4i552K48hYP67JkGob_zic2tny5hjbyRH8vXELbbsTPL4qz089VAQAA.zip"
     goto mi
 )
-if %errorlevel% == 4 goto c
+if %errorlevel% == 4 (
+    set "version=1.0.0.28"
+    set "downloadUrl=https://iknow-dl.service.hihonor.com/ctkbfm/servlet/download/downloadServlet/H4sIAAAAAAAAAD1QTU-EMBD9K6YnTcim0NKWPQllMR5wTXaNRzMLBRv52AwgcY3_3WJwM4fJm7w3b-Z9k2kwePw6G7IlAfFI2c_dCoWDlW3ME7QLfHAthw5qgzf-hroK1G3XN31995ZZbGdAs7nY86p6hvHdqZisWEFpJGVouApBncJAgfCrKBSiAN-xT_byWDrqYX__SinlivPFukADo-27o13sqUcGW3cwTrgco4RMYx1IlfAok5LrJE6oZqlWUaCFzHjo83SnUxowR82EYGzHfCmoyJjUPHX7ZxgN5oAfWQM12XZT03jEIPaYD1fcQhGXJZph-J98QmPLl2toI07m74U1tMOe_PwCOwyzElUBAAA%3D.zip"
+)
 if %errorlevel% == 5 goto c
 if %errorlevel% == 6 goto c
 if %errorlevel% == 7 goto c
@@ -362,7 +475,7 @@ if %errorlevel% == 16 goto c
 goto c
 
 :p
-@REM 打开项目仓库
+@REM 打开项目仓库释放
 cls
 echo =========================
 echo 请选择要打开哪个平台的项目仓库:
@@ -400,5 +513,6 @@ goto c
 
 :x
 @REM 退出
+color 02
 timeout /t 3
 exit
